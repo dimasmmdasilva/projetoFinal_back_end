592217acab67d24592dc5ffa81f31e1ea9253c91
Dimas_Marcondes <dimasmarcondes88@gmail.com>
2024-10-15 12:37:58 -0300

    correção sistema de autentificação

--- twitter_corujinha/settings.py
+++ twitter_corujinha/settings.py
@@ -117,5 +117,19 @@ CORS_ALLOWED_ORIGINS = [
     "https://twitter-corujinha.onrender.com",
 ]
 
+# Habilitar credenciais e configurar os headers permitidos
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # Tipo de campo padrão para chaves primárias
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
