11d38da2b49865a088ca43d8c7ca318ecbeaa2b3
Dimas_Marcondes <dimasmarcondes88@gmail.com>
2024-11-11 18:30:38 -0300

    updating to RENDER...

--- twitter_corujinha/core/views/updateImage.py
+++ twitter_corujinha/core/views/updateImage.py
@@ -1,31 +1,28 @@
-import logging
+import os
+from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from ..serializers import CurrentUserSerializer
 
-logger = logging.getLogger(__name__)
-
 class UpdateProfileImageView(APIView):
     def patch(self, request):
-        logger.debug("Iniciando o processo de upload da imagem de perfil.")
-        
-        # Obtém o usuário autenticado e o arquivo de imagem enviado
         user = request.user
         profile_image = request.FILES.get('profile_image')
 
         if not profile_image:
-            logger.error("Nenhuma imagem de perfil fornecida.")
             return Response({"detail": "Nenhuma imagem fornecida."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Atualiza o campo de imagem no usuário e salva
-        user.profile_image = profile_image
-        user.save()
+        # Define o caminho do diretório onde as imagens serão salvas
+        profile_images_path = os.path.join(settings.MEDIA_ROOT, 'profile_images')
+        
+        # Verifica se a pasta profile_images existe dentro de media; se não, cria
+        if not os.path.exists(profile_images_path):
+            os.makedirs(profile_images_path)
 
-        logger.debug("Imagem de perfil atualizada no banco de dados e armazenada na pasta media/profile_images.")
+        # Atualiza o campo de imagem no usuário e salva a imagem no diretório `profile_images`
+        user.profile_image.save(f"profile_images/{profile_image.name}", profile_image, save=True)
 
         # Retorna os dados do usuário com a nova URL da imagem
         serializer = CurrentUserSerializer(user, context={'request': request})
-        logger.debug("Processo de upload concluído com sucesso.")
-        
         return Response(serializer.data, status=status.HTTP_200_OK)
